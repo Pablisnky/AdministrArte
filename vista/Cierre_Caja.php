dd57eb927fe4bfa2595f410ef5724ee02da01ecf
PabloCabeza <pcabeza7@gmail.com>
2019-07-10 21:46:05 -0500

    Se realizaron cambios en la identación HTML

--- vista/Cierre_Caja.php
+++ vista/Cierre_Caja.php
@@ -13,31 +13,37 @@
 		<meta name="MobileOptimized" content="width">
 		<meta name="HandheldFriendly" content="true">
 
-		<link rel="stylesheet" type="text/css" href="css/EstilosAdministrArte.css">
+		<link rel="stylesheet" type="text/css" href="../css/Estilos_AdministrArte.css">
         <link rel="stylesheet" type="text/css" media="(max-width: 800px)" href="css/MediaQuery_EstilosAdministrArte_Movil.css"> 
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"><!-- Estilos calendario-->
 
         <script src="../javascript/funcionesVarias.js"></script> 
         <script src="../javascript/funciones_Ajax.js"></script> 
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script><!--Libreria del Calendario-->
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script><!--Libreria del Calendario-->
+        <script src="../javascript/CalendarioJQwery_2.js"></script> <!--Formato Calendario-->
 	</head>
-	<body onload="llamar_mostrarCierreCaja()">
+	<body onload="llamar_mostrarCierreCaja()"><!--() llamar_mostrarCierreCaja() se encuentran en funciones_Ajax.js -->
 		<div id="Principal">
-			<h1 class="anula">AdministrArte</h1>
-			<p id="Texto_1" class="texto_1c">Sistema de gestión administrativa de ImpresiónArte</p>
-            <img class="imagen_8" alt="Logo de ImpresiónArte" src="images/LogoImpresionArte.jpg">
-            <a href="../index.html">Regresar</a>
+			<?php 
+				include("../modulos/header.php");
+			?>
 		</div>
 		<div>
 			<form action="../controlador/recibe_cierre.php" method="POST" autocomplete="off">
-				<fieldset>
+				<fieldset class="fieldset_1">
 					<legend>Insertar monto de cierre</legend>
 					<label>fecha</label>
-					<input type="date" name="fecha" id="Fecha">
+					<input type="text" name="fecha" id="Calendario_CM_2">
 					<br>
 					<label>Monto</label>
 					<input type="text" name="monto">
 					<br>
 					<input type="submit" value="Cargar">
 				</fieldset>
-			</form>
+			</form> 
 		</div>
-		<div id="MostrarGastos"></div><!-- Muestra resultados de petición Ajax, trae respuesta de mostrarGastos.php-->
+		<div id="MostrarCierreCaja"></div><!-- Muestra resultados de petición Ajax, trae respuesta de mostrarCierreCaja.php-->
+		<a href="../index.php">Regresar</a>
+	</body>
+</html>
