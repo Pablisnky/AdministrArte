7bbca657ecec93a6dd047ad8ccb7351fe02fe498
PabloCabeza <pcabeza7@gmail.com>
2019-07-10 22:27:54 -0500

    Se añade la función calcular_Transferencia(). en esta función el valor de la variable "A" no pudo ser insertado por medio de getElementById("")

--- javascript/funcionesVarias.js
+++ javascript/funcionesVarias.js
@@ -1,30 +1,26 @@
-function MostrarCaja_1(){
-	var A= document.getElementById("Caja_1");
-	var B= document.getElementById("Caja_2");
-	if(A.style.display == "none"){
-		A.style.display = "block";		
-		A.style.transitionDuration= "2s";
-	}
-	else if(A.style.display == "none"){
-		alert("hola");
-		//A.style.display= "none";
-	}	
-	else if(B.style.display == "block"){
-		B.style.display= "none";
-	}
+//Desactiva los radio button de tiempo en caso de que se dese introducir tiempo manualmente, llamada desde Estampados.js
+function DesactivaRadio(){
+	var radios = document.formEstam.tiempo;
+
+	for(var i=0, iLen=radios.length; i<iLen; i++){
+		  radios[i].checked = false;
+	} 
 }
 
-function MostrarCaja_2(){
-	var A= document.getElementById("Caja_1");
-	var B= document.getElementById("Caja_2");
-	if(B.style.display == "none"){
-		B.style.display = "block";
-	}
-	else if(B.style.display == "block"){
-		alert("hola");
-		//A.style.display= "none";
-	}	
-	else{
-		alert("Error");
-	}
+//Desactiva el input con tiempo manual en caso de activar tiempo desde radio butoom, llamada desde Estampados.js
+function DesactivaTiemManual(){
+	document.getElementById("TiempoVariado").value="";	
+}
+
+//llamada desde Transferencia.php
+function calcular_Transferencia(){
+	var A= document.Form_Transferencia.montoGiro.value;
+	var Transferir= A / 0.48;
+
+	//Se formatean el monto con separación de miles y dos decimales 
+	var options1 = { style: 'decimal', maximumFractionDigits: 2,};
+	var numberFormat1 = new Intl.NumberFormat('de-DE', options1);
+	MontoTransferencia= numberFormat1.format(Transferir);
+	
+	var B= document.Form_Transferencia.montoTransferido.value= MontoTransferencia;
 }
