840721a427e50ac39d18fa82925ae6e07ce38cb8
PabloCabeza <pcabeza7@gmail.com>
2019-07-10 21:55:55 -0500

    Se corrige la identación HTML

--- modelo/mostrarGastos.php
+++ modelo/mostrarGastos.php
@@ -7,8 +7,8 @@
 	//se busca en la BD todos los gastos de la empresa
 	$Consulta= "SELECT * FROM gastos ORDER BY Fecha DESC";
 	$Recordset= mysqli_query($Conexion, $Consulta);
-	?>
-			<div style="float: left;">
+?>
+		<div style="float: left;">
 			<table>
 				<thead>
 					<th>FECHA</th>
@@ -17,25 +17,33 @@
 				</thead>
 				<tbody>
 					<?php 
-					while($Resultado= mysqli_fetch_array($Recordset)){  ?>	
+					while($Resultado= mysqli_fetch_array($Recordset)){
+
+						//Se formatean el monto con separación de miles y dos decimales  
+						$MontoCierre= number_format($Resultado["Monto"], 2, ",", ".")?>	
 						<tr>
 							<td><?php echo $Resultado["Fecha"];?></td>
 							<td><?php echo $Resultado["Descripcion"];?></td>
-							<td><?php echo $Resultado["Monto"];?></td>
+							<td><?php echo $MontoCierre;?></td>
 						</tr>   
 						<?php
 					}  ?>
 				</tbody>
 			</table>  
-			</div>		
-			<div style="float: right; margin-right: 50%;">
+		</div>		
+		<div style="float: right; margin-right: 50%;">
 			<?php
 			// Se consulta cuanto se gasto en el mes
 			$Consulta_2= "SELECT SUM(monto) AS Total FROM gastos";
 			$Recordset_2= mysqli_query($Conexion, $Consulta_2);
 			$Resultado_2= mysqli_fetch_array($Recordset_2);
 			$Total= $Resultado_2["Total"];
-			echo "TOTAL= " . $Total;
-			?>
-			</div>
+
+			//Se formatean el monto con separación de miles y dos decimales 
+			$Monto= number_format($Total, 2, ",", ".");
+			echo "TOTAL= " . $Monto;
+				?>
+		</div>
+
+			
 
