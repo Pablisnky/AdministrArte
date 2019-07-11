a7117e07cfb88b0c8ac0ab3b76db1b67e1fa2fd1
PabloCabeza <pcabeza7@gmail.com>
2019-07-10 22:39:44 -0500

    Se corrige identación en código HTML

--- controlador/recibe_cierre.php
+++ controlador/recibe_cierre.php
@@ -2,12 +2,15 @@
 	$Fecha= $_POST["fecha"];
 	$Monto= $_POST["monto"];
 
-	// echo "Fecha= " . $Fecha . "<br>";
-	// echo "Monto= " . $Monto . "<br>";
+	echo "Fecha= " . $Fecha . "<br>";
+	echo "Monto= " . $Monto . "<br>";
 
+	//se cambia el formato de la fecha antes de introducira a la base de datos para que mysql la pueda reconocer
+	$fechaFormatoMySQL = date("Y-m-d", strtotime($Fecha));
+	
 	include("../conexion/Conexion_BD.php");
 
-	$Insertar= "INSERT INTO cierre_caja (Fecha_Cierre, Monto_Cierre) VALUES('$Fecha','$Monto')";
+	$Insertar= "INSERT INTO cierre_caja (Fecha_Cierre, Monto_Cierre) VALUES('$fechaFormatoMySQL','$Monto')";
 	mysqli_query($Conexion, $Insertar) or die ( "Algo ha ido mal en la consulta a la base de datos");
 
 	header("Location:../vista/Cierre_Caja.php");
