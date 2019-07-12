7751fb966588ee369dea478d33d4bebea77b3c7a
PabloCabeza <pcabeza7@gmail.com>
2019-07-12 11:34:40 -0500

    S e crearon las funciones Detalles(), configurarTasa(), Calcular(), transferencia() y divisa()

--- javascript/funcionesVarias.js
+++ javascript/funcionesVarias.js
@@ -7,20 +7,68 @@ function DesactivaRadio(){
 	} 
 }
 
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
 //Desactiva el input con tiempo manual en caso de activar tiempo desde radio butoom, llamada desde Estampados.js
 function DesactivaTiemManual(){
 	document.getElementById("TiempoVariado").value="";	
 }
 
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
 //llamada desde Transferencia.php
 function calcular_Transferencia(){
 	var A= document.Form_Transferencia.montoGiro.value;
-	var Transferir= A / 0.48;
-
-	//Se formatean el monto con separación de miles y dos decimales 
-	var options1 = { style: 'decimal', maximumFractionDigits: 2,};
-	var numberFormat1 = new Intl.NumberFormat('de-DE', options1);
-	MontoTransferencia= numberFormat1.format(Transferir);
+	var B= document.getElementById("COP_Bs");
+	var C= document.getElementById("Bs_COP");
+	var E= document.getElementById("TasaProveedor").value;
 	
-	var B= document.Form_Transferencia.montoTransferido.value= MontoTransferencia;
+	if(B.checked==true){
+		var Transferir= A / E;
+		//Se formatean el monto con separación de miles y dos decimales 
+		var options1 = { style: 'decimal', maximumFractionDigits: 2,};
+		var numberFormat1 = new Intl.NumberFormat('de-DE', options1);
+		var MontoTransferencia= numberFormat1.format(Transferir);
+		var D= document.Form_Transferencia.Resultado.value= MontoTransferencia;
+		document.getElementById("Resultado").style.display="Block";
+	}
+	else{
+		var Transferir= A * E;
+		//Se formatean el monto con separación de miles y dos decimales 
+		var options1 = { style: 'decimal', maximumFractionDigits: 2,};
+		var numberFormat1 = new Intl.NumberFormat('de-DE', options1);
+		var MontoTransferencia= numberFormat1.format(Transferir);
+		var D= document.Form_Transferencia.Resultado.value= MontoTransferencia;
+		document.getElementById("Resultado").style.display="Block";
+	}
+}
+
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+//Llamada desde Transferencia.php
+function Divisa_1(){
+	var A= document.getElementById("Bs_COP");
+	var B= document.getElementById("MontoGiro");
+	var C= document.getElementById("MontoTransferido");
+
+	if(A.checked==true){
+		C.style.display = "block";
+		B.style.visibility="false"
+	}
+}
+
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+//Llamada desde mostrarTransferencia.php
+function Detalles(id){
+	// alert(id);
+	var CI= document.getElementById(id).value;
+	window.open("../controlador/detallesTrans.php?Cedula=" + CI,"anexo","width=800px,height=600px,left=300px");
+}
+
+//-----------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------
+//Llamada desde Transferencia.php
+function configurarTasa(){
+	window.open("../vista/configurarTasa.php","anexo","width=800px,height=600px,left=300px");
 }
