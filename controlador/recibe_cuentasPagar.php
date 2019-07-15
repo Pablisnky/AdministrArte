6dd53d8d40332847de53fd912fd6e16ccb184c3b
PabloCabeza <pcabeza7@gmail.com>
2019-07-14 20:52:36 -0500

    Se adicionaron otras variables que llegan por POST y se insertan en la BD

--- controlador/recibe_cuentasPagar.php
+++ controlador/recibe_cuentasPagar.php
@@ -3,11 +3,20 @@
 	$Descripcion= $_POST["descripcion"];
 	$Acreedor= $_POST["acreedor"];
 	$Monto= $_POST["monto"];
+	$NumeroCuotas= $_POST["numeroCuotas"];
+	$MontoCuota= $_POST["montoCuota"];
+	$Pagos= $_POST["pagos"];
+	$Interes= $_POST["interes"];
+	$FechaPago= $_POST["fecha_pagos"];
 
-	// echo "Fecha= " . $Fecha . "<br>";
-	// echo "Descripcion= " . $Descripcion . "<br>";
-	// echo "Acreedor= " . $Acreedor . "<br>";
-	// echo "Monto= " . $Monto . "<br>";
+	echo "Fecha= " . $Fecha . "<br>";
+	echo "Descripcion= " . $Descripcion . "<br>";
+	echo "Acreedor= " . $Acreedor . "<br>";
+	echo "Monto= " . $Monto . "<br>";
+	echo "NumeroCuotas= " .$NumeroCuotas . "<br>";
+	echo "MontoCuota= " . $MontoCuota . "<br>";
+	echo "Pagos= " . $Pagos . "<br>";
+	echo "Interes= " . $Interes . "<br>";
 
 	//se cambia el formato de la fecha antes de introducira a la base de datos para que mysql la pueda reconocer
     $fechaFormatoMySQL = date("Y-m-d", strtotime($Fecha));
@@ -15,8 +24,8 @@
 
 	include("../conexion/Conexion_BD.php");
 
-	$Insertar= "INSERT INTO cuentas_pagar(Fecha, Descripcion, Acreedor, Monto) VALUES('$fechaFormatoMySQL', '$Descripcion', '$Acreedor', '$Monto')";
+	$Insertar= "INSERT INTO cuentas_pagar(Fecha, Descripcion, Acreedor, Monto, Numero_Cuotas, Monto_cuota, Pagos, Interes, Fecha_pago) VALUES('$fechaFormatoMySQL', '$Descripcion', '$Acreedor', '$Monto', '$NumeroCuotas', '$MontoCuota', '$Pagos', '$Interes','$FechaPago')";
 	mysqli_query($Conexion, $Insertar) or die ("Algo ha ido mal en la consulta a la base de datos");
 
-	header("Location:../vista/Gastos.php");
+	header("Location:../vista/Cuentas_Pagar.php");
 ?>
