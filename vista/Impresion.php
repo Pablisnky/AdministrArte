47acf95b155f779e3a89d8be74f12ee196e574b9
PabloCabeza <pcabeza7@gmail.com>
2019-07-14 21:03:16 -0500

    Se mejoró código HTML y se quitó el action del formulario, no era es necesario, los datos son enviados a un archivo ajax para obtner una respuesta inmediata que no será almacenada

--- vista/Impresion.php
+++ vista/Impresion.php
@@ -26,30 +26,29 @@
 			?>
 		</div>
 		<div>
-			<form action="../controlador/recibe_impresion.php" method="POST" autocomplete="off" name="formImpre" id="FormImpre">
+			<form autocomplete="off" name="formImpre" id="FormImpre">
 				<fieldset class="fieldset_1">
-					<legend>Seleccione tipo papel</legend>
-					<input type="radio" name="tipo" id="Bond" value="Bond">
+					<legend class="legend_2">Seleccione tipo papel</legend>
+					<input type="radio" name="papel" id="Bond" value="Bond">
 					<label for="Bond">Bond</label>
 					<br>
-					<input type="radio" name="tipo" id="BondAdhesivo" value="BondAdhesivo">
+					<input type="radio" name="papel" id="BondAdhesivo" value="BondAdhesivo">
 					<label for="BondAdhesivo">Bond adhesivo</label>
 					<br>
-					<input type="radio" name="tipo" id="Opalina" value="Opalina">
+					<input type="radio" name="papel" id="Opalina" value="Opalina">
 					<label for="Opalina">Opalina</label>
 					<br>
-					<input type="radio" name="tipo" id="Propalcote" value="Propalcote">
+					<input type="radio" name="papel" id="Propalcote" value="Propalcote">
 					<label for="Propalcote">Propalcote</label>
 					<br>
-					<input type="radio" name="tipo" id="Periodico" value="Periodico">
+					<input type="radio" name="papel" id="Periodico" value="Periodico">
 					<label for="Periodico">Periodico</label>
 					<br>
-					<input type="radio" name="tipo" id="Kraft" value="Kraft">
+					<input type="radio" name="papel" id="Kraft" value="Kraft">
 					<label for="Kraft">Kraft</label>
-					<br>
 				</fieldset>
 				<fieldset class="fieldset_1">
-					<legend>Seleccione tamaño papel</legend>
+					<legend class="legend_2">Seleccione tamaño papel</legend>
 					<input type="radio" name="tamaño" id="A0" value="A0">
 					<label for="A0">A0</label>
 					<br>
@@ -71,7 +70,7 @@
 					<input type="radio" name="tamaño" id="Cuarto" value="Cuarto">
 					<label for="Cuarto">Cuarto pliego</label>
 					<br>
-					<input type="radio" name="tamaño" id="Octavo" value="Oficio">
+					<input type="radio" name="tamaño" id="Octavo" value="Octavo">
 					<label for="Octavo">Octavo pliego</label>
 					<br>
 					<input type="radio" name="tamaño" id="Oficio" value="Oficio">
@@ -79,24 +78,54 @@
 					<br>
 					<input type="radio" name="tamaño" id="Carta" value="Carta">
 					<label for="Carta">Carta</label>
-					<br>
 				</fieldset>
 				<fieldset class="fieldset_1"> 
-					<legend>Ajuste de color</legend>
+					<legend class="legend_2">Ajuste de color</legend>
 					<input type="radio" name="color" id="Color" value="color">
 					<label for="Color">Color</label>
 					<br>
 					<input type="radio" name="color" id="Monocromatico" value="monocromatico">
 					<label for="Monocromatico">Monocromatico</label>
+				</fieldset>
+				<fieldset class="fieldset_1"> 
+					<legend class="legend_2">Porcentaje de color</legend>
+					<input type="radio" name="P_color" id="PC_10" value="10">
+					<label for="PC_10">10 %</label>
 					<br>
+					<input type="radio" name="P_color" id="PC_20" value="20">
+					<label for="PC_20">20 %</label>
+					<br>
+					<input type="radio" name="P_color" id="PC_30" value="30">
+					<label for="PC_30">30 %</label>
+					<br>
+					<input type="radio" name="P_color" id="PC_40" value="40">
+					<label for="PC_40">40 %</label>
+					<br>
+					<input type="radio" name="P_color" id="PC_50" value="50">
+					<label for="PC_50">50 %</label>
+					<br>
+					<input type="radio" name="P_color" id="PC_60" value="60">
+					<label for="PC_60">60 %</label>
+					<br>
+					<input type="radio" name="P_color" id="PC_70" value="70">
+					<label for="PC_70">70 %</label>
+					<br>
+					<input type="radio" name="P_color" id="PC_80" value="80">
+					<label for="PC_80">80 %</label>
+					<br>
+					<input type="radio" name="P_color" id="PC_90" value="90">
+					<label for="PC_90">90 %</label>
+					<br>
+					<input type="radio" name="P_color" id="PC_100" value="100">
+					<label for="PC_100">100 %</label>
 				</fieldset>
 				<fieldset class="fieldset_1">
-					<legend>Descuento al mayor</legend>
+					<legend class="legend_2">Descuento</legend>
 				</fieldset>
 					<br>
-					<input type="submit" value="Presupuestar">
+					<input type="button" value="Calcular" onclick="llamar_PresupuestoPlotter()">
 			</form>
 		</div>
-		<div id="MostrarImpresion"></div><!-- Muestra resultados de petición Ajax, trae respuesta de mostrarGastos.php-->
+		<div id="MostrarImpresion"></div><!-- Muestra resultados de petición Ajax, trae respuesta de mostrarImpresion.php-->
         <a class="a_1" href="../index.php">Regresar</a>
 
