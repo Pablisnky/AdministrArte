e4c947083727c3a546d68e099a266f40789604be
PabloCabeza <pcabeza7@gmail.com>
2019-07-14 20:55:50 -0500

    Se adicionaron más controles al formulario.

--- vista/Cuentas_Pagar.php
+++ vista/Cuentas_Pagar.php
@@ -13,17 +13,17 @@
 		<meta name="MobileOptimized" content="width">
 		<meta name="HandheldFriendly" content="true">
 
-		<link rel="stylesheet" type="text/css" href="css/EstilosAdministrArte.css">
+		<link rel="stylesheet" type="text/css" href="../css/Estilos_AdministrArte.css">
         <link rel="stylesheet" type="text/css" media="(max-width: 800px)" href="css/MediaQuery_EstilosAdministrArte_Movil.css"> 
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"><!-- Estilos calendario-->
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"><!--Estilos calendario-->
 
         <script src="../javascript/funcionesVarias.js"></script> 
         <script src="../javascript/funciones_Ajax.js"></script> 
-        <!-- <script src="https://code.jquery.com/jquery-1.12.4.js"></script><!--Libreria del Calendario-->
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script><!--Libreria del Calendario-->
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script><!--Libreria del Calendario-->
-        <script src="../javascript/CalendarioJQwery.js"></script> Formato Calendario -->
+    	<script src="../javascript/CalendarioJQwery.js"></script><!-- Formato Calendario -->
 	</head>
-	<body onload=""><!--llamar_cuentasPagar() llamar_cuentasPagar()se encuentra en funciones_Ajax.js -->
+	<body onload="llamar_mostrarCuentasPagar()">
 		<div id="Principal">
 			<?php 
 				include("../modulos/header.php");
@@ -31,30 +31,67 @@
 		</div>
 		<div>
 			<form action="../controlador/recibe_cuentasPagar.php" method="POST" autocomplete="off">
-				<fieldset>
-					<legend>Insertar nuevo deuda</legend>
+				<fieldset class="fieldset_2">
+					<legend>Insertar nueva deuda</legend>
 					<table>
 						<thead>
 							<th>Fecha</th>
                             <th>Descripción</th>
                             <th>Acreedor</th>
 							<th>Monto</th>
+							<th>Nº Cuotas</th>
+							<th>Monto cuota</th>
+							<th>Pagos</th>
+							<th>Interes (%)</th>
+							<th>Fecha pago</th>
 						</thead>
 						<tbody>
 							<tr>
-								<td><input type="date" name="fecha" id="Calendario_CM"></td>
+								<td><input type="text" name="fecha" id="Calendario_CM"></td>
 								<td><textarea name="descripcion"></textarea></td>
 								<td><textarea name="acreedor"></textarea></td>
-                                <td><input type="text" name="monto" id="Monto"></td>
-                                <td><input type="text" id="<?php echo $A;?>" value="<?php echo $A;?>" hidden onclick="Detalles_CuentasPagar(this.id)"><label for="<?php echo $A;?>">Detalles</label></td>   
+								<td><input type="text" name="monto" id="Monto"></td> 
+								<td>
+									<select name="numeroCuotas">
+										<option></option>
+										<option>1</option>
+										<option>2</option>
+										<option>3</option>
+										<option>4</option>
+										<option>5</option>
+										<option>6</option>
+										<option>7</option>
+										<option>8</option>
+										<option>9</option>
+										<option>10</option>
+										<option>11</option>
+										<option>12</option>
+									</select>
+								</td> 
+								<td><input type="text" name="montoCuota"></td> 
+								<td>
+									<select name="pagos">
+										<option></option>
+										<option>semanal</option>
+										<option>quincenal</option>
+										<option>mensual</option>
+									</select></td> 
+								<td><input type="text" name="interes"></td> 
+								<td><input type="text" name="fecha_pagos"></td> 
 							</tr>
 						</tbody>
 					</table>
+					<input type="text" id="EnvioAjax" value="EnvioAjax_CuentasPagar" hidden="hidden">
+					<!--solo para enviar a ajax,  -->
 					<input type="submit" value="Cargar">
 				</fieldset>
 			</form>
 		</div>
-		<div id="MostrarGastos"></div><!-- Muestra resultados de petición Ajax, trae respuesta de mostrarGastos.php-->
+		<div id="MostrarCuentas_Pagar"></div><!-- Muestra resultados de petición Ajax, trae respuesta de mostrarCuentasPagar.php-->
 		<a href="../index.php">Regresar</a>
+		<br>
+		<a href="">Registrar Pago</a><!--Abre un formulario para registrar el pago de una cuota o el pago total de la deuda-->
 	</body>
 </html>
+
+<!--llamar_mostrarCuentasPagar() se encuentra en funciones_Ajax.js -->
